f23461ad5078719e569d27adadefb21fed4790ec
Dilara <73229391+dilaraismn@users.noreply.github.com>
2023-05-02 14:45:39 +0300

    NavMesh - Off Mesh Link And Jump

--- Assets/_Scripts/ExampleScripts/NavAgentExample.cs
+++ Assets/_Scripts/ExampleScripts/NavAgentExample.cs
@@ -12,6 +12,9 @@ public class NavAgentExample : MonoBehaviour
    public int CurrentIndex = 0;
    public bool HasPath = false;
    public bool PathPending = false;
+   public bool PathStale = false;
+   public NavMeshPathStatus PathStatus = NavMeshPathStatus.PathInvalid;
+   public AnimationCurve JumpCurve = new AnimationCurve();
 
    private void Start()
    {
@@ -29,19 +32,15 @@ public class NavAgentExample : MonoBehaviour
       int incStep = increment ? 1 : 0;
       Transform nextWaypointTransform = null;
 
-      while (nextWaypointTransform == null)
+      int nextWaypoint = (CurrentIndex + incStep >= WaypointNetwork.Waypoints.Count) ? 0 : CurrentIndex + incStep;
+      nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
+
+      if (nextWaypointTransform != null)
       {
-         int nextWaypoint = (CurrentIndex + incStep >= WaypointNetwork.Waypoints.Count) ? 0 : CurrentIndex + incStep;
-         nextWaypointTransform = WaypointNetwork.Waypoints[nextWaypoint];
-
-         if (nextWaypointTransform != null)
-         {
-            CurrentIndex = nextWaypoint;
-            _navAgent.destination = nextWaypointTransform.position;
-            return;
-         }
+         CurrentIndex = nextWaypoint;
+         _navAgent.destination = nextWaypointTransform.position;
+         return;
       }
-
       CurrentIndex++;
    }
 
@@ -49,10 +48,39 @@ public class NavAgentExample : MonoBehaviour
    {
       HasPath = _navAgent.hasPath;
       PathPending = _navAgent.pathPending;
+      PathStale = _navAgent.isPathStale;
+      PathStatus = _navAgent.pathStatus;
 
-      if (!HasPath && !PathPending)
+      if (_navAgent.isOnOffMeshLink)
+      {
+         StartCoroutine(Jump(1));
+         return;
+      }
+      
+      if ((_navAgent.remainingDistance <= _navAgent.stoppingDistance && !PathPending)|| PathStatus == NavMeshPathStatus.PathInvalid /*||PathStatus == NavMeshPathStatus.PathPartial*/)
       {
          SetNextDestination(true);
       }
+      else if (_navAgent.isPathStale)
+      {
+         SetNextDestination(false);
+      }
+   }
+
+   IEnumerator Jump(float duration)
+   {
+      OffMeshLinkData data = _navAgent.currentOffMeshLinkData;
+      Vector3 startPos = _navAgent.transform.position;
+      Vector3 endPos = data.endPos + (_navAgent.baseOffset * Vector3.up);
+      float time = 0;
+
+      while (time <= duration)
+      {
+         float t = time / duration;
+         _navAgent.transform.position = Vector3.Lerp(startPos, endPos, t) + JumpCurve.Evaluate(t) * Vector3.up;
+         time += Time.deltaTime;
+         yield return null;
+      }
+      _navAgent.CompleteOffMeshLink();
    }
 }
