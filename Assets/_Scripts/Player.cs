8a9f25db908369e6156b9ee473bc695afe545113
Dilara <dilaraismn@gmail.com>
2023-05-11 01:40:08 +0300

    Midterm Version Completed

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float shootRate = 0.2f;
-    [SerializeField] private float shootForce = 2000;
+    [SerializeField] private float shootForce;
     [SerializeField] private GameObject bulletPref;
     [SerializeField] private Transform bulletPoint;
-
+    [SerializeField] private Camera playerCamera;
     public bool canShoot = true; //TODO
     
+
     void Update()
     {
         if (Input.GetMouseButton(0) && canShoot)
@@ -21,8 +23,20 @@ public class Player : MonoBehaviour
 
     private void Shoot()
     {
-        GameObject bullet = Instantiate(bulletPref, bulletPoint.transform.position, bulletPref.transform.rotation);
-        bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        Ray ray = playerCamera.ScreenPointToRay(screenCenter);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 direction = (hit.point - transform.position).normalized;
+
+            GameObject bullet = Instantiate(bulletPref, bulletPoint.transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().AddForce(direction * shootForce, ForceMode.Impulse);
+        }
+        
+        //GameObject bullet = Instantiate(bulletPref, bulletPoint.transform.position, bulletPref.transform.rotation);
+        //bullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
         //TODO: AudioSource.PlayClipAtPoint(shootSound, transform.position); 
 
         canShoot = false;
