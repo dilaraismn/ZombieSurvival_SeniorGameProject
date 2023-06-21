6375647d385f99dcd24e1374f556a361c3cdf0fa
Dilara <73229391+dilaraismn@users.noreply.github.com>
2023-06-21 15:46:13 +0300

    Zombie: Dead bug fixs & WIP sfx

--- Assets/_Scripts/Zombie.cs
+++ Assets/_Scripts/Zombie.cs
@@ -11,23 +11,44 @@ public class Zombie : MonoBehaviour
     [SerializeField] private GameObject bloodVFX;
     [SerializeField] private int zombieHealth = 100;
     [SerializeField] private int bulletDamage = 25;
+    [SerializeField] private AudioClip growl, bite, run, getHit, dead;
 
     private Animator animator;
     private NavAgentExample navAgentScript;
     private NavMeshAgent navAgent;
     private CapsuleCollider collider;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         navAgent = GetComponent<NavMeshAgent>();
         navAgentScript = GetComponent<NavAgentExample>();
+        collider = GetComponent<CapsuleCollider>();
+        audioSource = GetComponent<AudioSource>();
     }
-    
+
+    private void Start()
+    {
+        //audioSource.playOnAwake = true;
+        audioSource.loop = true;
+        audioSource.clip = growl;
+        audioSource.Play();
+    }
+
+    public void ChangeSX(AudioClip clipToPlay)
+    {
+        audioSource.playOnAwake = false;
+        audioSource.loop = false;
+        audioSource.clip = clipToPlay;
+        audioSource.Play();
+    }
+
     private void TakeDamage()
     {
         zombieHealth -= bulletDamage;
         navAgentScript.enabled = false;
+        ChangeSX(getHit);
 
         if (zombieHealth <= 0)
         {
@@ -37,6 +58,7 @@ public class Zombie : MonoBehaviour
         else
         {
             animator.Play("TakeHit");
+            ChangeSX(run);
             navAgent.ResetPath();
             navAgent.SetDestination(playerObj.position);
         }
@@ -45,7 +67,11 @@ public class Zombie : MonoBehaviour
     private void Die()
     {
         animator.Play("Die");
+        ChangeSX(dead);
+        navAgentScript.enabled = false;
+        navAgent.enabled = false;
         collider.enabled = false;
+        gameObject.isStatic = true;
     }
     
     private void OnTriggerEnter(Collider other)
@@ -55,6 +81,11 @@ public class Zombie : MonoBehaviour
             Instantiate(bloodVFX, other.transform.position, Quaternion.identity);
             Destroy(other.gameObject);
             TakeDamage();
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            ChangeSX(bite);
         }
     }
 }
